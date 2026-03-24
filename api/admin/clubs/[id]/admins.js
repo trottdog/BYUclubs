154250db3ead33c7ddd52209393e2cdcf4c0dae0
Gunnjake <Gunnjakeim@gmail.com>
2026-03-23 23:03:41 -0600

    Revert "fix super admin route resolution in production"
    This reverts commit 456387ad96f44cb15a49dfe8450635fe42d73a44.

--- api/admin/clubs/[id]/admins.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  runtime: "nodejs",
-};
-
-export default async function handler(req, res) {
-  const { default: app } = await import("../../../../../artifacts/api-server/dist/app.mjs");
-  return app(req, res);
-}
